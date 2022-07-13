73635da055dc345243dabe93a56565c2ab29f1d4
unknown <sand8594@hufs.ac.kr>
2022-07-13 20:45:50 +0900

    오류로 인한 BrowserRouter 수정

--- crypto-tracker/src/Router.tsx
+++ crypto-tracker/src/Router.tsx
@@ -8,13 +8,13 @@ interface Imode {
     toggleDarkMode: Function,
 }
 
+
 function Router({isDarkMode, toggleDarkMode} : Imode) {
-    
     return (
-        <BrowserRouter>
+        <BrowserRouter basename = {process.env.PUBLIC_URL}>
             <Routes>
                 <Route path = "/" element={<Coins isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode}/>}/>
-                <Route path = "/:coinId/*" element={<Coin isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode}/>}/>
+                <Route path = "/:coinId" element={<Coin isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode}/>}/>
             </Routes>
         </BrowserRouter>
             
